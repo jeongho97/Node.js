4936561af458c15bea646b2ef7f2ec6331a3dd21
이정호 <skjongho@naver.com>
2022-05-03 15:50:30 +0900

    MongoDB node.js connect

--- conn_MongoDB.js
+++ conn_MongoDB.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const MongoClient=require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb+srv://jeongho0304:jo73002745!@cluster0.4g5jy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function(err,client){
+MongoClient.connect('mongodb+srv://jeongho0304:<password>@cluster0.4g5jy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function(err,client){
     if(err) return console.log(err)
     app.listen(5000,function(){
         console.log("listening on 5000")
